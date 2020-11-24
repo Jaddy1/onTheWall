c47e625020579e6ee4705c0d853b080f3fe6b94b
Jeffron <Jeffron>
2020-11-24 00:52:25 -0500

    project folder unzipped

---
+++ finalproject/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+application = app
+bootstrap = Bootstrap(app)
+
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+@app.route('/signup')
+def signup():
+	return render_template('signup.html')
+
+@app.route('/login')
+def login():
+	return render_template('login.html')
